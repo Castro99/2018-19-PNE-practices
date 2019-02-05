8bd82e7bb562e1c4829443fa86147bc0a3650ad8
Castro99 <david99esp@hotmail.es>
2019-02-05 17:06:30 +0100

    Python exercise

--- P0/fibonacci.py
+++ P0/fibonacci.py
@@ -1,12 +1,8 @@
 #Exercise of fibonacci I
 
-num = int(input('Enter desire num to compute the fibonacci series: '))
-
-
 def fibonacci_series(num):
     value = 0
-    x = 0
-    y = 1
+    x,y = 0,1
     fibonacci_series=[]
 
 
@@ -17,6 +13,9 @@ def fibonacci_series(num):
         y = z
         value += 1
     return fibonacci_series
+
+
+num = int(input('Enter desire num to compute the fibonacci series: '))
 
 
 print(fibonacci_series(num))
