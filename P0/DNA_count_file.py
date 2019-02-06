e04c96472d50c866686eb4a9d78d6f2185ece856
Castro99 <david99esp@hotmail.es>
2019-02-06 13:50:23 +0100

    Python exercise

---
+++ P0/DNA_count_file.py
@@ -0,0 +1,22 @@
+#Exercise DNA II
+
+with open('DNA.csv', 'r') as f:
+    for series in f:
+        def dna_length():
+            number = len(series.replace('\n', ''))
+            a = series.count('a')
+            c = series.count('c')
+            g = series.count('g')
+            t = series.count('t')
+            return number, a, c, g, t
+
+        info = dna_length()
+        def print_dna_length(number):
+            print('Length: ', number[0])
+            print('a: ', number[1])
+            print('c: ', number[2])
+            print('g: ', number[3])
+            print('t: ', number[4])
+            return
+
+        print(print_dna_length(info))
