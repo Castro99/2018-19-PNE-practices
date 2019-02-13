8b1470ddc6ae4cdcb8cb05dcbb7853085d18a326
Castro99 <david99esp@hotmail.es>
2019-02-13 17:30:29 +0100

    Python exercise

---
+++ Session-7/Exercise-2.py
@@ -0,0 +1,20 @@
+# Programming our first client
+import socket
+
+while True:
+    # Create a socket for communicating with the server
+    s = socket.socket(socket. AF_INET, socket.SOCK_STREAM)
+
+    print("The Socket has been created")
+
+    PORT = 8080
+    IP = "212.128.253.64"
+    # Connect to server
+    s.connect((IP, PORT))
+
+    # Encode convert a string to bytes
+    s.send(str.encode(input("WRITE A MESSAGE: ")))
+
+    msg = s.recv(2048).decode("utf-8")
+    print("THE SERVER MESSAGE")
+    print(msg)
