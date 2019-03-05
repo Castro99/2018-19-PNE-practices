20326c9aeba8f93552d947a7c3e852110bde10d2
Castro99 <david99esphotmail.es>
2019-03-05 15:30:21 +0100

    Python exercise

---
+++ Session-5/EX_2.py
@@ -0,0 +1,28 @@
+#Exercise II lesson 5
+
+class Seq:
+    """A class for representing sequences"""
+    def __init__(self, strbases):
+        print("New sequence created!")
+
+        self.strbases = strbases
+
+    def len(self):
+        return len(self.strbases)
+
+class Gene(Seq):
+
+s1 = Gene("ATTCGATCC")
+s2 = Seq("AAAGG")
+
+str1 = s1.strbases
+str2 = s2.strbases
+
+l1 = s1.len()
+l2 = s2.len()
+
+print("First Sequence: {}".format(str1))
+print("First Length: {}".format(l1))
+print("Second Seuqnece: {}".format(str2))
+print("Second Length: ·{}".format(l2))
+
