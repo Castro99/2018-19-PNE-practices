0644df4aad01ea82e6030ae1ce7de9fffe605ad1
Castro99 <david99esphotmail.es>
2019-03-05 16:15:11 +0100

    Python exercise

---
+++ P2/Ex_1.py
@@ -0,0 +1,41 @@
+#Exercise I lesson P2
+class Seq:
+
+    def  __init__  (self, strbases):
+
+        self.strbases = strbases
+    def len(self):
+        return len(self.strbases)
+
+    def complement(self):
+        word = ''
+        for letter in self.strbases:
+            self.strbases = self.strbases.upper()
+            if letter == 'A':
+                word += 'T'
+            elif letter == 'C':
+                word += 'G'
+            elif letter == 'G':
+                word += 'C'
+            elif letter == 'T':
+                word += 'A'
+        c = Seq(word)
+        return c
+
+    def reversed(self):
+        s = self.strbases[::-1]
+        seq = Seq(s)
+        return seq
+
+
+
+
+    def counting(self, base):
+        self.base = base
+        count = self.strbases.count(base)
+        return count
+
+    def percentage(self, base):
+        self.base = base
+        p = round(100.0 * self.strbases.count(base) / len(self.strbases), 2)
+        return p
