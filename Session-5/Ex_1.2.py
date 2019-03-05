42eb3100941c53869fd8318e9626e15f53d167f3
Castro99 <david99esphotmail.es>
2019-03-05 15:54:16 +0100

    Python exercise

---
+++ Session-5/Ex_1.2.py
@@ -0,0 +1,37 @@
+from Base import count_base
+
+def percentages(seq):
+    # Calculate the total length
+    tl = len(s)
+    a, c, g, t = seq.count('A'), seq.count('C'), seq.count('G'), seq.count('T')
+    variables = [a, c, g, t]
+    astats, cstats, gstats, tstats = 0, 0, 0, 0
+    stats = [astats, cstats, gstats, tstats]
+    for i, elem in zip(variables, range(4)):
+        stats[elem] = round(100.0 * i / tl, 1)
+    return stats
+
+
+def main(s):
+    count= count_base(s)
+    stats= percentages(s)
+    print('Base A')
+    print('Counter: ', count['A'])
+    print('Percentage: ', stats[0])
+
+    print('Base C')
+    print('Counter: ', count['C'])
+    print('Percentage: ', stats[1])
+
+    print('Base G')
+    print('Counter: ', count['G'])
+    print('Percentage: ', stats[2])
+
+    print('Base T')
+    print('Counter:  ', count['T'])
+    print('Percentage: ', stats[3])
+
+
+s= input('Enter a sequence: ')
+s=s.upper()
+main(s)
