d1d824047c01c4940305ed98c0a4d3e22065399d
Castro99 <david99esp@hotmail.es>
2019-03-18 17:38:48 +0100

    Python exercise

---
+++ P3/Client.py
@@ -0,0 +1,18 @@
+#Exercise Client lesson P3
+
+    import socket
+
+    port = 8046
+    IP = "192.168.1.134"
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((IP, port))
+    msg = """
+    """
+
+    s.send(msg.encode())
+
+    message = s.recv(2048).decode('utf-8')
+    print(message)
+
+    s.close()
