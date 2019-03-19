999727246bf41a23d22ed057000fb1de5189b87d
Castro99 <david99esp@hotmail.es>
2019-03-19 13:05:08 +0100

    Python exercise

---
+++ P0-Extra/CPLX2_count.py
@@ -0,0 +1,12 @@
+#Exercise III P0-Extra
+
+with open('CPLX2.txt', 'r') as l:
+    a, c, t, g = 0, 0, 0, 0
+    for x in l:
+        if '>' in x:
+            x=''
+        a += x.count('A')
+        c += x.count('C')
+        t += x.count('T')
+        g += x.count('G')
+print('A: ', a, '\nC: ', c, '\nT: ', t,'\nG: ', g)
