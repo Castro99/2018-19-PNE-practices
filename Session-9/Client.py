1d3abae5922b7600033014fb4d5c4316255aa13f
Castro99 <david99esp@hotmail.es>
2019-03-22 13:37:51 +0100

    Python exercise

---
+++ Session-9/Client.py
@@ -0,0 +1,26 @@
+#Exercise Client Session 9
+
+import socket
+
+IP = "192.168.1.134"
+PORT = 8080
+
+while True:
+    # Client blocking the server
+    msg = input(">")
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # establishing connection to the Server
+    s.connect((IP, PORT))
+
+    # Sending request message to the server
+    s.send(str.encode(msg))
+
+    # Receive servers response
+    response = s.recv(2048).decode()
+
+    # Print response
+    print("Response: {}".format(response))
+
+    s.close()
