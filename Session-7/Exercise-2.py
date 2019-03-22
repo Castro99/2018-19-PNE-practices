803211f94e00f4378eaad1a9ed0dfba29bffc950
Castro99 <david99esp@hotmail.es>
2019-03-22 13:41:32 +0100

    Correct IP

--- Session-7/Exercise-2.py
+++ Session-7/Exercise-2.py
@@ -1,18 +1,22 @@
-# Programming our first client
+# Programming our first client Exercise 2 Session 7
+
 import socket
 
 while True:
     # Create a socket for communicating with the server
+
     s = socket.socket(socket. AF_INET, socket.SOCK_STREAM)
 
     print("The Socket has been created")
 
     PORT = 8080
-    IP = "212.128.253.64"
+    IP = "192.168.1.134"
     # Connect to server
+
     s.connect((IP, PORT))
 
     # Encode convert a string to bytes
+
     s.send(str.encode(input("WRITE A MESSAGE: ")))
 
     msg = s.recv(2048).decode("utf-8")
