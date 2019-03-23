c420b41a766b1f932104caadda662ee154eb2af5
Castro99 <david99esp@hotmail.es>
2019-03-23 21:12:56 +0100

    Python exercise

---
+++ Session-9/Exercise_1_server.py
@@ -0,0 +1,46 @@
+import socket
+import termcolor
+
+PORT = 8081
+IP = "192.168.1.134"
+
+MAX_OPEN_REQUEST = 5
+
+
+def process_client(cs):
+
+    msg = cs.recv(2048).decode("utf-8")
+    if msg == "Exit":
+        cs.send(str.encode(" The server has finished"))
+
+        cs.close()
+        return False
+    msg_print = termcolor.colored(msg, 'green')
+    print("Message from the client: {}".format(msg_print))
+
+    msg_send = termcolor.colored(msg, 'grey')
+    cs.send(str.encode(msg_send))
+
+    cs.close()
+    return True
+
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+serversocket.bind((IP, PORT))
+
+serversocket.listen(MAX_OPEN_REQUEST)
+
+print("Socket ready: {}".format(serversocket))
+active = True
+while active:
+
+    print("Waiting for connection at: {}, {}".format(IP, PORT))
+    (clientsocket, address) = serversocket.accept()
+
+    if process_client(clientsocket):
+
+
+        print("Attending client: {}".format(address))
+    else:
+        active = False
