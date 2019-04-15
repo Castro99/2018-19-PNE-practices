737bd927141b254dbe837031513c6744370d87f0
Castro99 <david99esp@hotmail.es>
2019-04-15 15:33:39 +0200

    Python exercise

--- P1/Ex_1.main.py
+++ P1/Ex_1.main.py
@@ -2,31 +2,31 @@
 
 from Ex_1 import Seq
 
-seq1 = Seq("ACTGAATTCTTGC")
-seq2 = Seq("CCTGA")
-seq3 = Seq.complement(seq1)
-seq4 = Seq.reversed(seq3)
+Sequence1 = Seq("ACTGAATTCTTGC")
+Sequence2 = Seq("CCTGA")
+Sequence3 = Seq.compl(Sequence1)
+Sequence4 = Seq.rev(Sequence3)
 
 
-print("First Sequence is: {}".format(seq1))
-print("Length is: {}".format(Seq.len(seq1)))
-print("Bases count: A:{}, C:{}, T:{}, G:{}".format(seq1.counting("A"), seq1.counting("C"), seq1.counting("T"), seq1.counting("G")))
-print("Bases percentage: A:{}, C:{}, T:{}, G:{}".format(seq1.percentage("A"), seq1.percentage("C"), seq1.percentage("T"), seq1.percentage("G")))
+print("1º Sequence: {}".format(Sequence1))
+print("Length for 1º Sequence: {}".format(Seq.len(Sequence1)))
+print("Bases count for 1º Sequence: A:{}, C:{}, T:{}, G:{}".format(Sequence1.Count("A"), Sequence1.Count("C"), Sequence1.Count("T"), Sequence1.Count("G")))
+print("Bases percentage for 1º Sequence: A:{}, C:{}, T:{}, G:{}".format(Sequence1.percent("A"), Sequence1.percent("C"), Sequence1.percent("T"), Sequence1.percent("G")))
 
 
-print("Second Sequence is: {}".format(seq2))
-print("Length is: {}".format(Seq.len(seq2)))
-print("Bases count: A:{}, C:{}, T:{}, G:{}".format(seq2.counting("A"), seq2.counting("C"), seq2.counting("T"), seq2.counting("G")))
-print("Bases percentage: A:{}, C:{}, T:{}, G:{}".format(seq2.percentage("A"), seq1.percentage("C"), seq2.percentage("T"), seq2.percentage("G")))
+print("2º Sequence: {}".format(Sequence2))
+print("Length for 2º Sequence: {}".format(Seq.len(Sequence2)))
+print("Bases count: A:{}, C:{}, T:{}, G:{}".format(Sequence2.Count("A"), Sequence2.Count("C"), Sequence2.Count("T"), Sequence2.Count("G")))
+print("Bases percentage: A:{}, C:{}, T:{}, G:{}".format(Sequence2.percent("A"), Sequence2.percent("C"), Sequence2.percent("T"), Sequence2.percent("G")))
 
 
-print("Third Sequence is: {}".format(seq3))
-print("Length is: {}".format(Seq.len(seq3)))
-print("Bases count: A:{}, C:{}, T:{}, G:{}".format(seq3.counting("A"), seq3.counting("C"), seq3.counting("T"), seq3.counting("G")))
-print("Bases percentage: A:{}, C:{}, T:{}, G:{}".format(seq3.percentage("A"), seq3.percentage("C"), seq3.percentage("T"), seq3.percentage("G")))
+print("Third Sequence is: {}".format(Sequence3))
+print("Length is: {}".format(Seq.len(Sequence3)))
+print("Bases count: A:{}, C:{}, T:{}, G:{}".format(Sequence3.Count("A"), Sequence3.Count("C"), Sequence3.Count("T"), Sequence3.Count("G")))
+print("Bases percentage: A:{}, C:{}, T:{}, G:{}".format(Sequence3.percent("A"), Sequence3.percent("C"), Sequence3.percent("T"), Sequence3.percent("G")))
 
 
-print("Fourth Sequence is: {}".format(seq4))
-print("Length is: {}".format(Seq.len(seq4)))
-print("Bases count: A:{}, C:{}, T:{}, G:{}".format(seq4.counting("A"), seq4.counting("C"), seq4.counting("T"), seq4.counting("G")))
-print("Bases percentage: A:{}, C:{}, T:{}, G:{}".format(seq4.percentage("A"), seq4.percentage("C"), seq4.percentage("T"), seq4.percentage("G")))
+print("Fourth Sequence is: {}".format(Sequence4))
+print("Length is: {}".format(Seq.len(Sequence4)))
+print("Bases count: A:{}, C:{}, T:{}, G:{}".format(Sequence4.Count("A"), Sequence4.Count("C"), Sequence4.Count("T"), Sequence4.Count("G")))
+print("Bases percentage: A:{}, C:{}, T:{}, G:{}".format(Sequence4.percent("A"), Sequence4.percent("C"), Sequence4.percent("T"), Sequence4.percent("G")))
