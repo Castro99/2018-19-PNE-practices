3571733ecd6a2723f38fc8f5ad96c0d2225bd50a
Castro99 <david99esp@hotmail.es>
2019-05-27 17:41:36 +0200

    Python exercise

--- P6/Seq.py
+++ P6/Seq.py
@@ -1,27 +1,40 @@
 #Exercise Sequence lesson P6
-
 class Seq:
+    """A class for representing sequences"""
 
     def __init__(self, strbases):
+        # this method is called every time a new object is created
+
         self.strbases = strbases
 
     def len(self):
-        return len(self.strbases)
+        return str(len(self.strbases))
+
+    def complement(self):
+        seq = self.strbases
+        seq = seq.upper()
+        for index, elem in enumerate(seq):
+            if elem == 'A':
+                seq = seq[:index] + seq[index:].replace('A', 'T')
+            if elem == 'T':
+                seq = seq[:index] + seq[index:].replace('T', 'A')
+            if elem == 'C':
+                seq = seq[:index] + seq[index:].replace('C', 'G')
+            if elem == 'G':
+                seq = seq[:index] + seq[index:].replace('G', 'C')
+        return seq
+
+    def reverse(self):
+        seq = self.strbases.upper()[::-1]
+        return seq
 
     def count(self, base):
-        n_base = self.strbases.count(base)
-        return n_base
+        base=base.upper()
+        return str(self.strbases.upper().count(base))
 
     def perc(self, base):
-        tl = len(self.strbases)
-        if tl > 0:
-            nbase = self.count(base)
-            perc = round(100.0 * nbase / tl, 1)
-        else:
-            perc = 0
-        return perc
-
-    def call_function(self, name, base=""):
-        fn = getattr(self, name)
-        value = fn(base)
-        return value
+        seq = self.strbases.upper()
+        base=base.upper()
+        counter= seq.count(base)
+        tl = len(seq)
+        return str(round(100.0 * counter/tl, 2))
