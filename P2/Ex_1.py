8ce502c757be030165623fca06e869e38ca0096a
Castro99 <david99esp@hotmail.es>
2019-05-27 17:07:57 +0200

    Python exercise

--- P2/Ex_1.py
+++ P2/Ex_1.py
@@ -1,41 +1,42 @@
 #Exercise I lesson P2
 class Seq:
+    """A class for representing sequences"""
 
-    def  __init__  (self, strbases):
+    def __init__(self, strbases):
+        # this method is called every time a new object is created
 
         self.strbases = strbases
+
     def len(self):
         return len(self.strbases)
 
-    def complement(self):
-        word = ''
-        for letter in self.strbases:
-            self.strbases = self.strbases.upper()
-            if letter == 'A':
-                word += 'T'
-            elif letter == 'C':
-                word += 'G'
-            elif letter == 'G':
-                word += 'C'
-            elif letter == 'T':
-                word += 'A'
-        c = Seq(word)
-        return c
 
-    def reversed(self):
-        s = self.strbases[::-1]
-        seq = Seq(s)
+    def complement(self):
+        seq = self.strbases
+        seq = seq.upper()
+        for index, elem in enumerate(seq):
+            if elem == 'A':
+                seq = seq[:index] + seq[index:].replace('A', 'T')
+            if elem == 'T':
+                seq = seq[:index] + seq[index:].replace('T', 'A')
+            if elem == 'C':
+                seq = seq[:index] + seq[index:].replace('C', 'G')
+            if elem == 'G':
+                seq = seq[:index] + seq[index:].replace('G', 'C')
         return seq
 
 
+    def reverse(self):
+        seq = self.strbases.upper()[::-1]
+        return seq
 
+    def count(self, base):
+        base=base.upper()
+        return self.strbases.upper().count(base)
 
-    def counting(self, base):
-        self.base = base
-        count = self.strbases.count(base)
-        return count
-
-    def percentage(self, base):
-        self.base = base
-        p = round(100.0 * self.strbases.count(base) / len(self.strbases), 2)
-        return p
+    def perc(self, base):
+        seq = self.strbases.upper()
+        base=base.upper()
+        counter= seq.count(base)
+        tl = len(seq)
+        return round(100.0 * counter/tl, 1)
