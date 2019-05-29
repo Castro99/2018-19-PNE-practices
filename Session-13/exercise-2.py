7dad6a040c8eaeb64d1463cb25352ae2df1c70ba
Castro99 <david99esp@hotmail.es>
2019-05-29 17:14:16 +0200

    Python exercise

---
+++ Session-13/exercise-2.py
@@ -0,0 +1,44 @@
+import http.server
+import socketserver
+
+PORT = 8009
+
+class TestHandler (http.server.BaseHTTPRequestHandler):
+
+    #function called whenever we have a request
+    def do_GET(self):
+        print('GET received')
+
+        print('Request line:' + self.requestline)
+        print(' Cmd: '+ self.command)
+        print(' Path: ' + self.path)
+
+        content=''
+
+        if self.path == '/':
+            with open('index.html', 'r') as f:
+                for line in f:
+                    content += line
+
+        else:
+            with open('error.html', 'r') as f:
+                for line in f:
+                    content += line
+
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Length-Type', len(str.encode(content)))
+        self.end_headers()
+
+        self.wfile.write(str.encode(content))
+
+        return
+
+# function that's called whenever there's a request
+Handler = TestHandler
+
+# if you don't write any IP, you use yours
+with socketserver.TCPServer(("",PORT), Handler) as httpd:
+    print('Serving at PORT',PORT)
+
+    httpd.serve_forever()
