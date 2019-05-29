8d46fcd413356f3a06924fe2d653989f6d5bb184
Castro99 <david99esp@hotmail.es>
2019-05-29 03:47:19 +0200

    Python exercise

--- P0/fibonacci_sum.py
+++ P0/fibonacci_sum.py
@@ -1,28 +1,10 @@
 #Exercise of fibonacci II
 
-def fibonacci_series(num):
-    value = 0
-    x,y = 0,1
-    fibonacci_series=[]
-
-
-    while value < num:
-        fibonacci_series.append(x)
-        z = x + y
-        y = z
-        x = y
-        value += 1
-    return fibonacci_series
-
-
-num = int(input('Enter desire num to compute the fibonacci series: '))
-
-def fibonacci_series_total(series):
-    final_fibonacci_series = 0
-    for i in series:
-        final_fibonacci_series += i
-    return final_fibonacci_series
-
-total=fibonacci_series_total(fibonacci_series(num))
-
-print("The final output of fibonacci series is: ", total)
+x = 0
+y = 1
+counter=0
+n=int(input('Enter desire num to compute the fibonacci series: '))
+for i in range(n):
+   x, y = y, x+y
+   counter += x
+print(counter)
